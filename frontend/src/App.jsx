d3b29e6d9da0b977d0f7aeb1bb3f8ff610758449
Abdullah Zia <abziawork960@gmail.com>
2024-05-19 15:07:21 +0500

    basic setup chakra UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,28 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 const App = () => {
-  return <Button colorScheme="blue">HI</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: ` "nav" "main" `,
+        lg: ` "nav" "main" `,
+      }}
+    >
+      {/* <Show above="lg">
+      <GridItem area="nav" bg="dodgerblue">
+        NavBar
+      </GridItem>
+      </Show> */}
+
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <GridItem area="main" bg="coral">
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
